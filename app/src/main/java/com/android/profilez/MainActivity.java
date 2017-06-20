52126b337986ba201e2b80a2a381c719506cfb74
Cristian <cristiangjuan@gmail.com>
2017-06-20 19:13:49 +0200

    Initial commit

---
+++ app/src/main/java/com/android/profilez/MainActivity.java
@@ -0,0 +1,95 @@
+package com.android.profilez;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.widget.FrameLayout;
+
+
+import com.android.profilez.utils.Constants;
+
+public class MainActivity extends FragmentActivity {
+
+
+    protected Context mContext;
+    private FrameLayout mainLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	Log.v(Constants.Log.METHOD, "MainActivity - OnCreate");
+
+		getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+
+        super.onCreate(savedInstanceState);
+
+        mContext = this;
+
+        setContentView(R.layout.main_layout);
+        mainLayout = (FrameLayout) findViewById(R.id.main_layout);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+	protected void onPause() {
+
+    	Log.v(Constants.Log.METHOD, "MainActivity - OnPause");
+
+		super.onPause();
+	}
+
+    @Override
+    protected void onRestart() {
+        Log.v(Constants.Log.METHOD, "MainActivity - onRestart");
+
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+
+        Log.v(Constants.Log.METHOD, "MainActivity - onStop");
+
+        super.onStop();
+    }
+
+	@Override
+	protected void onDestroy() {
+		
+		Log.v(Constants.Log.METHOD, "MainActivity - OnDestroy");
+		
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		Log.v(Constants.Log.METHOD, "MainActivity - OnResume");
+
+		super.onResume();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		Log.v(Constants.Log.METHOD, "MainActivity - onSaveInstanceState");
+		super.onSaveInstanceState(outState);
+	}
+	
+    @Override
+    public void onBackPressed() {
+    	
+    	Log.v(Constants.Log.METHOD, "MainActivity - onBackPressed");
+
+
+    }
+
+}
---
+++ app/src/main/java/com/android/profilez/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.android.profilez.utils;
+
+public class Constants {
+
+	public static final int SCROLL_FACTOR = 2;
+	public static final String IMAGE_DATA_EXTRA = "resId";
+	
+	
+	public class Log {
+		
+		public static final String METHOD = "Meth";
+		public static final String STORE = "Store";
+		public static final String DEBUG = "Dbug";
+		public static final String CONTROLS = "Ctrl";
+		public static final String TOUCH = "Tuch";
+		public static final String TIMER = "Time";
+		public static final String SCROLL = "Scrol";
+		public static final String HTTP = "HTTP";
+	}
+}
